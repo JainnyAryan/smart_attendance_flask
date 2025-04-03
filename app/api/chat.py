c9638799af3db049a7ae5b76584ee5f70f21e3a1
Aryan Jain <aryan.one0793@gmail.com>
2025-04-03 18:46:35 +0530

    projects with chat

--- app/api/chat.py
+++ app/api/chat.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 import requests
 from fastapi import APIRouter, Depends
-from app.crud.chat import get_attendance_stats
+from app.crud.chat import get_all_projects, get_attendance_stats
 from app.database import get_db
 from app.schemas.chat import ChatResponse, ChatRequest
+from app.schemas.project import ProjectResponse
 
 
 router = APIRouter()
@@ -25,11 +26,12 @@ async def chat_endpoint(request: ChatRequest, db=Depends(get_db)):
     entities = chatbot_data["entities"]
     category = chatbot_data["category"]
 
-    if confidence < 0.3:
+    if confidence < 0.3 and confidence > 0:
         return {"response": "Sorry, I didn\'t understand that.",  "confidence": confidence, "intent": intent, "data": None}
 
     if category == "greetings":
         return {"response": msg_response, "confidence": confidence, "intent": intent, "data": None}
+
     if intent == "employee_attendance_summary":
         response_msg = msg_response
         attd_data = None
@@ -42,10 +44,17 @@ async def chat_endpoint(request: ChatRequest, db=Depends(get_db)):
                     continue
                 response_msg = msg_response.format(ent["name"])
                 break
-        
         if not attd_data:
             return {"response": f"Couldn't fetch attendance data. Please try entering exact full name...", "confidence": confidence, "intent": intent, "data": None}
-
         return {"response": response_msg, "confidence": confidence, "intent": intent, "data": attd_data}
+
+    elif intent == "all_projects":
+        response_msg = msg_response
+        projects_data = None
+        projects_data = get_all_projects(db=db)
+        projects_data = [ProjectResponse.model_validate(project).model_dump() for project in projects_data]
+        if not projects_data:
+            return {"response": "Couldn't fetch ongoing projects data.", "confidence": confidence, "intent": intent, "data": None}
+        return {"response": response_msg, "confidence": confidence, "intent": intent, "data": {"projects": projects_data}}
 
     return {"response": "I'm still learning! I'll improve over time.",  "confidence": confidence, "intent": intent, "data": None}
--- app/crud/chat.py
+++ app/crud/chat.py
@@ -4,6 +4,8 @@ from app.crud.attendance import get_attendance_calendar_data
 from app.models.employee import Employee
 from datetime import datetime, timedelta
 
+from app.models.project import Project
+
 def get_attendance_stats(db: Session, name: str):
     res = db.query(Employee).filter(func.lower(Employee.name).ilike(name))
     emp = res.first()
@@ -15,3 +17,8 @@ def get_attendance_stats(db: Session, name: str):
     start_date = (datetime.today().date() - timedelta(days=30)).strftime("%Y-%m-%d")  # Convert to string
 
     return get_attendance_calendar_data(db=db, emp_id=emp.id, start_date=start_date, end_date=end_date)
+
+
+def get_all_projects(db: Session):
+    res = db.query(Project).order_by(Project.created_at.desc()).all()
+    return res
